9678de3276d1e727e7758260b085d67cf1e247b9
Jeremias Adriano <109630558+jeremiasadriano@users.noreply.github.com>
2024-04-16 15:28:53 +0200

    I created a htmx file to use AJAX without jquery

--- Emotions/Emotions/urls.py
+++ Emotions/Emotions/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from Person.urls import view
 
 urlpatterns = [
+    path('',view.chat, name='home'),
     path('',include('Person.urls')),
     path('admin/', admin.site.urls)
 ]
